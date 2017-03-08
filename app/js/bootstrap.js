e31c15c93e394c80cca1efb3126aac58f3c1339a
Michael Lee <mlee@vellichorlabs.com>
2017-03-07 22:32:41 -0900

    Added test components

---
+++ app/js/bootstrap.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_component_1 = require("./app.component");
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent).catch(function (err) { return console.log(err); });
+//# sourceMappingURL=bootstrap.js.map
---
+++ app/js/bootstrap.ts
@@ -0,0 +1,4 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent).catch(err => console.log(err));
